e2617d67c7ef4bf008373185493283bef3c4cc9b
Marlon Chalegre <marlon.chalegre@nubank.com.br>
2024-03-02 16:00:24 -0300

    Use a capacitive soil sensor

--- frida-life-support.ino
+++ frida-life-support.ino
@@ -5,15 +5,15 @@
 
 #define pinoAnalog A0
 #define pinoRele 8
-#define pinoSensor 7
-
-int ValAnalogIn;
 
 byte ledState = LOW;
 
 unsigned long previousMillis = 0;
 unsigned long intervalOff = 7200000;  //desligado por 2h
 int intervalOffInMinutes = (int)((intervalOff / 1000) / 60);
+
+const int dry = 518;  // value for dry sensor
+const int wet = 215;  // value for wet sensor
 
 unsigned long irrigationCounter = 0;
 
@@ -31,20 +31,36 @@ byte waterDropChar[8] = {
 };
 
 
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(pinoRele, OUTPUT);
+
+  lcd.init();
+  lcd.setBacklight(HIGH);
+
+  lcd.createChar(0, waterDropChar);
+
+  lcd.setCursor(5, 0);
+  lcd.print("Prox.");
+  lcd.setCursor(0, 1);
+  lcd.print("Qtd Irr. ");
+
+  previousMillis = millis() - intervalOff;
+}
+
 int readSensor() {
   int mesuresCount = 0;
   int percentage = 0;
-
-  digitalWrite(pinoSensor, HIGH);
 
   lcd.setCursor(0, 0);
   lcd.write((byte)0);
   lcd.print("..");
   lcd.print("%");
 
-  while (mesuresCount < 30) {
-    ValAnalogIn = analogRead(pinoAnalog);
-    int currentValue = map(ValAnalogIn, 1023, 0, 0, 100);
+  while (mesuresCount < 5) {
+    int valAnalogIn = analogRead(pinoAnalog);
+    int currentValue = map(valAnalogIn, wet, dry, 100, 0);
 
     percentage = (currentValue + percentage) / 2;
 
@@ -60,35 +76,13 @@ int readSensor() {
   lcd.print(percentage);
   lcd.print("%");
 
-  digitalWrite(pinoSensor, LOW);
-
   return percentage;
-}
-
-
-void setup() {
-  Serial.begin(9600);
-
-  pinMode(pinoRele, OUTPUT);
-  pinMode(pinoSensor, OUTPUT);
-
-  lcd.init();
-  lcd.setBacklight(HIGH);
-
-  lcd.createChar(0, waterDropChar);
-
-  doIrrigacao();
-
-  lcd.setCursor(5, 0);
-  lcd.print("Prox.");
-  lcd.setCursor(0, 1);
-  lcd.print("Qtd Irr. ");
 }
 
 void doIrrigacao() {
   int porcento = readSensor();
 
-  while (porcento <= 47) {
+  while (porcento <= 45) {
     Serial.println("Irrigando a planta ...");
     digitalWrite(pinoRele, HIGH);
 
